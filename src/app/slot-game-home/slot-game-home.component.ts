bc60fe358cc2da1321dff44a84d842049017ab3c
Ankita6993-eng <ankitaa.tagline@gmail.com>
2021-04-21 17:32:46 +0530

    second

--- src/app/slot-game-home/slot-game-home.component.ts
+++ src/app/slot-game-home/slot-game-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit ,ViewChild} from '@angular/core';
+import { Component, OnInit ,ViewChild,ElementRef} from '@angular/core';
 import {AnimationBuilder, trigger,
   state,
   style,
@@ -6,24 +6,33 @@ import {AnimationBuilder, trigger,
   transition,} from '@angular/animations';
  import { ChartType, ChartOptions } from 'chart.js';
 import { SingleDataSet, Label, monkeyPatchChartJsLegend, monkeyPatchChartJsTooltip,BaseChartDirective } from 'ng2-charts';
-import { Observable, of } from 'rxjs';
 
 
 @Component({
   selector: 'app-slot-game-home',
   templateUrl: './slot-game-home.component.html',
   styleUrls: ['./slot-game-home.component.css'],
-  animations:[
-    trigger('btncaseout',[
-      state('move', style({
-        transform: 'translateX(300px) ',
-      })),
-      transition('state=>move',animate('3000ms ease')),
-    ])
-  ]
+   animations: [
+    trigger('btnState', [
+      state(
+        'move',
+        style({
+          transform: 'translate3d(300px,50px,0)',
+        })
+      ),
+      state(
+        'state2',
+        style({
+          transform: 'translate3d(300px,50px,0)',
+        })
+      ),
 
+      transition('state=>state2', animate('1s ease-in')),
+      transition('state2=>state',animate('1s ease-in')),
+   ]),
+
+  ],
 })
-
 export class SlotGameHomeComponent implements OnInit {
  
    symbolReel:any[] = [{
@@ -46,27 +55,38 @@ export class SlotGameHomeComponent implements OnInit {
     credits:40
   }];
  @ViewChild(BaseChartDirective) chart: BaseChartDirective;
+  @ViewChild('panel') public panel:ElementRef;
   currentSym1:any;
   currentSym2: any;
   currentSym3: any;
   user:string|any
   user_name:string|any
   credits: number|any;
-  position:any
+
   slot1_eq_slot2: boolean=false
   slot2_eq_slot3: boolean=false
   slot1_eq_slot3: boolean=false
   stopSpin:any; 
   stopSpin1:any
   stopSpin2:any
+   isapin:boolean=false
   totaltry:number|any= 0;
   wintry:number|any=0;
+  position: string|any;
+  position1:string|any;
+  currentState:any
+  btnDisabled:boolean|any=false
+
    
-  public pieChartLabels: string|any = ["total try","Win try"];
+  public pieChartLabels: string|any = ["Total Try"," Success Try"];
   public data: number|any = [this.totaltry,this.wintry];
   public pieChartType: string|any = "pie";
 
-   isapin:boolean=false
+  
+
+     h:any 
+      w:any
+   
   
   
   constructor(private animationBuilder:AnimationBuilder) { }
@@ -81,34 +101,48 @@ export class SlotGameHomeComponent implements OnInit {
   }
 
 obj_spin1(){
-  this.isapin=true
-    if(this.credits > 0){
-        this.stopSpin=setInterval(()=> {this.currentSym1 = this.symbolReel[Math.floor(Math.random() * (this.symbolReel.length))];},20)
-        setTimeout(() => {clearInterval(this.stopSpin);}, 1000);
+   this.isapin=true
+    this.btnDisabled=false
+  if(this.credits>0){
+ this.stopSpin= setInterval(() => {
+        setTimeout(() => {this.currentSym1 = this.symbolReel[Math.floor(Math.random() * (this.symbolReel.length))]},1000);
+      }, 10);
 
-        this.stopSpin1=setInterval(()=> {this.currentSym2 = this.symbolReel[Math.floor(Math.random() * (this.symbolReel.length))];},20)
-        setTimeout(() => {clearInterval(this.stopSpin1);}, 2000);
+        setTimeout(() => {
+        clearInterval(this.stopSpin);
+      }, 1000);
 
-        this.stopSpin2=setInterval(()=> {this.currentSym3 = this.symbolReel[Math.floor(Math.random() * (this.symbolReel.length))];},20)
-        setTimeout(() => {clearInterval(this.stopSpin2); this.winOrLose()}, 3000);
+  this.stopSpin1=setInterval(() => {
+        setTimeout(() => {this.currentSym2  = this.symbolReel[Math.floor(Math.random() * (this.symbolReel.length))] },2000);
+      }, 10);
+      setTimeout(() => {
+        clearInterval(this.stopSpin1);
+      }, 2000);
 
-    }
-    else{
-      alert("Credits are No more . You have to restart");
-    } 
- this.totaltry++;
-this.chart_update()
-   
+  this.stopSpin2=setInterval(() => {
+        setTimeout(() => {this.currentSym3 = this.symbolReel[Math.floor(Math.random() * (this.symbolReel.length))] },3000);
+      }, 10 );
+      setTimeout(() => {
+        clearInterval(this.stopSpin2);
+      }, 3000);
+      setTimeout(()=>{this.winOrLose()},6500)
+  }
+else{
+  alert("Insufficent credits .. Restart game")
+}
+       this.totaltry++;
+ console.log("total try::",this.totaltry)
+ this.chart_update()
 }
 
-   winOrLose(){
-    
-     this.isapin=false
-    this.slot1_eq_slot2 = (this.currentSym1.value == this.currentSym2.value);
-    this.slot2_eq_slot3 = (this.currentSym2.value == this.currentSym3.value);
-    this.slot1_eq_slot3 = (this.currentSym1.value == this.currentSym3.value);
+   
+winOrLose(){    
+  this.isapin=false
+  this.slot1_eq_slot2 = (this.currentSym1.value == this.currentSym2.value);
+  this.slot2_eq_slot3 = (this.currentSym2.value == this.currentSym3.value);
+  this.slot1_eq_slot3 = (this.currentSym1.value == this.currentSym3.value);
 
-     if(this.slot1_eq_slot2 && this.slot2_eq_slot3 && this.slot1_eq_slot3){
+if(this.slot1_eq_slot2 && this.slot2_eq_slot3 && this.slot1_eq_slot3){
      
       if(this.slot1_eq_slot2){
         this.credits += this.currentSym1.credits ;
@@ -119,6 +153,7 @@ this.chart_update()
       }
       alert("You WON!");
       this.wintry++
+      console.log("success try::",this.wintry)
       this.chart_update()
     }else{
        this.credits--
@@ -126,23 +161,24 @@ this.chart_update()
      
     }
    }
-
    chart_update(){
         setTimeout(() => {
-            this.chart.chart.data.datasets[0].data=[this.totaltry,this.wintry]
-            this.chart.chart.update()
-        }, 20);  
+           this.chart.chart.data.datasets[0].data=[this.totaltry,this.wintry]
+            this.chart.update()
+        }, 50);  
    }
-
+disableButton(){
+    var rand =  Math.floor(Math.random() * 3);
+    setTimeout(() => {
+      this.btnDisabled= true;
+    }, rand * 3000);
+  }
    changePosition(){
-    this.position = "move"
+    this.position = 'state2';
+    this.currentState = this.currentState === 'initial'?'final':'initial';
+    this.panel.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    this.disableButton();
   }
-
-  stopSpinning(){
-    clearInterval(this.stopSpin);
-    this.winOrLose()
-  }
-
 
    existing:any = JSON.parse(localStorage.getItem('users') || '[]')
     onClick() {
@@ -156,10 +192,12 @@ this.chart_update()
             console.log("existing user====",this.existing) 
             localStorage.setItem('users', JSON.stringify( this.existing));
              localStorage.setItem('currentUser', JSON.stringify( this.user));
+             alert("Your credits now :"+this.credits)
       }
   }
   restart(){
     this.credits=10
+    this.isapin=false
      //this.onClick()
   
   } 
